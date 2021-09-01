93a95c78b7c745cc19df2f088d4ff05e9c91beba
zetakov <zetakov@eratosthen.es>
2021-09-01 01:15:54 -0400

    Added multiple options to androidxmrig

--- README.md
+++ README.md
@@ -9,29 +9,62 @@ Or the one we host
 
 The only different between the two is that **protomens** xmrig will be purposely kept back several commits from the **MoneroOcean** one. This is due to testing the new commits on Android Phones which can be time consuming. It is unlikely we will be making any changes to the code, but it could happen to create compatability with Android for new versions of xmrig. Just be mindful to check where each one is at on the branch. 
 
+**Note** androidxmrig.sh will give you the option to use the MoneroOcean xmrig or the one we have tested. We have not made any modifications at this time. 
+
 #### Why do I have to use Monero Ocean XMRig?
 Easy answer. Because of the algo-switching mechanisms, you'll contribute significantly more shares to the pool, hence receive more payments, then you would if you were to use the traditional XMRig on an Android phone. 
 
 #### What is sieve-rig?
-Really just XMRig. This repository just includes setup configs and any additional information to get sieve-rig (XMRig on Android) to work at it's optimal performance for the chipset. It also automates compiling and installing dependencies for xmrig within termux. Use of **termux-zetakov** is strongly recommended. 
+Really just XMRig. This repository just includes setup configs and any additional information to get sieve-rig (XMRig on Android) to work at it's optimal performance for the ARM chipset. It also automates compiling and installing dependencies for xmrig within termux. Use of **termux-zetakov** is strongly recommended. 
 
 ## Usage
-Download your choice of config file. Precomipiled phone ones i.e., *Pixel3a_config.json* or the standard *config.json* which will perform benchmarking for your device. It is recommended to use the precompiled version if you have the same device as this includes optimal settings for the rig. If a precompiled version doesn't exist use the traidtional *config.json* and go through the benchmarking.
+
+Download your choice of config file. Precomipiled phone ones i.e., *Pixel3a_config.json* or the standard *config.json* which will perform benchmarking for your device. It is recommended to use the precompiled version if you have the same device as this includes optimal settings for the rig. If a precompiled version doesn't exist use the traidtional *config.json* and go through the benchmarking. **Note** androidxmrig.sh will choose a generic config file and perform benchmarking for your specific device. 
 
 First copy `androidxmrig.sh` to your phone. Then in termux run the following:
 
 ```
 $ chmod +x androidxmrig.sh && ./androidxmrig.sh
+```
+
+The run: `./androidxmrig.sh -h` which outputs the options menu:
 
 ```
+ AndroidXmrig-mo v0.3.0  (Burt Bacharach)
+  
+ Usage:
+       ./androidxmrig.sh [options]
+  
+ Options: 
+           --update,    Update to the newsest stable release of xmrig-mo.
+           --mo,        Update to the newest and untested xmrig-mo release from MoneroOcean.
+           -h|--help,   This help screen.
+  
+```
+
+Run without any options, this will perform a fresh install on your phone. If you use the `--mo` flag this will git the MoneroOcean repo. Just note, this has not been tested by our crew and so we are unsure if it will compile on your phone. If you want a tried and tested **xmrig-mo** exclude the *--mo* flag. 
+
+If you already have compiled **xmrig** on your phone and would like to update to the newest version (tested or MoneroOcean) append the *--update* flag to the end of the command and this will rebuild **xmrig-mo** on your phone. 
+
+### Note (Fresh Install)
+
 This will install all necesary dependencies and compile **hwloc** and **xmrig-mo** on your phone. Follow the instructions and enter the necessary information when presented with a prompt.
 
 Easy peasy lemon squeezy. 
 
+
+## Phone specific config files
+
+Please note that we have taken the time to upload configuration files that have successfully worked on various phones. **androidxmrig.sh** will create a generic configuration file and do a benchmarking test to find the optimal configuration. If you would like to submit your configuration file to us to host on this repository, don't hesitate to make a pull request or send the file direction to sieve [at] eratosthen.es
+
+### Note
+
+If you run into xmrig quitting on you on a regular basis, it is advised that you find what algorithm your phone is optimized for (just look at the xmrig output), and append **~algoirthm** to the end of your **WorkerName** (which is the **pass** field in the configuration file)
+
 # Donation
 
 
-We are three strong minded individuals who developed SIEVE of Eratosthenes Technology - an all-encompassing Monero project. If you would like to support our cause, please donate to the following Monero Address. Any amount is welcomed!
+We are three strong minded individuals who developed SIEVE of Eratosthenes Technology - an immersive Monero project. If you would like to support our cause, as we are a donation only enterprise, please donate to the following Monero Address. Any amount is welcomed!
 
 ![https://www.getmonero.org](https://i.imgur.com/0x11LkT.png)
 
--- androidxmrig.sh
+++ androidxmrig.sh
@@ -1,43 +1,149 @@
 #!/bin/bash
 
-rm xmrig
-echo "We need to change the termux repo. Select (grimler) for best results on main repo"
-echo -ne "(Press enter to continue): "
-read blah
-sleep 1
-termux-change-repo
-echo " "
-echo -ne "Installing dependencies: (autoconf automake cmake git libtool)..."
-sleep 2
-apt-get -q -y install autoconf automake cmake git libtool 
-echo "Done."
-echo "Getting gits: (hwloc, xmrig, sieve-rig)..."
-mkdir git && cd git
-git clone https://github.com/protomens/hwloc
-git clone https://github.com/protomens/xmrig
-git clone https://github.com/zetakov/sieve-rig
-echo "Done"
-cd hwloc
-./autogen.sh && ./configure &&  make
-cd .. && cd xmrig && mkdir build && cd build
-cmake .. -DHWLOC_INCLUDE_DIR=/data/data/com.termux/files/home/git/hwloc/include -DHWLOC_LIBRARY=/data/data/com.termux/files/home/git/hwloc/hwloc/.libs/libhwloc.so
-make
-cp ../../sieve-rig/config.json .
-sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:20032",/' ./config.json
-echo -ne "XMR Address: "
-read XMRADDRESS
-sed -i 's/"user": *"[^"]*",/"user": "'$XMRADDRESS'",/' ./config.json
-echo -ne "Worker Name: "
-read WORKER
-sed -i 's/"pass": *"[^"]*",/"pass": "'"$WORKER"'",/' ./config.json
-sed -i 's/"tls": false,/"tls": true,/' ./config.json
-sed '/"tls":.*$/{n;s/false/true/}' -i ./config.json
-sed -i 's/"mode": *"[^"]*",/"mode": "lite",/' ./config.json
-cd /data/data/com.termux/files/home
-ln -s git/xmrig/build/xmrig xmrig
-echo "If all went as planned we will run xmrig-mo with benchmarking. If termux exits after benchmarking, either restart temux and xmrig or reboot the phone. This has to do with memory heaps being garbled during benchmarking."
-echo " "
-echo "Running xmrig...."
-sleep 4
-./xmrig
+UPDATE=0
+BETA=0
+GITDIR="/data/data/com.termux/files/home/git"
+HWLOCDIR="$GITDIR/hwloc"
+XMRIGDIR="$GITDIR/xmrig"
+
+
+
+help_screen() { 
+        echo " "
+        echo "AndroidXmrig-mo v0.3.0  (Burt Bacharach)"
+        echo " "
+        echo "Usage:"
+        echo "      ./androidxmrig.sh [options]"
+        echo " "
+        echo "Options: "
+        echo "          --update,    Update to the newsest stable release of xmrig-mo."
+        echo "          --mo,        Update to the newest and untested xmrig-mo release from MoneroOcean."
+        echo "          -h|--help,   This help screen."
+        echo " "
+        exit                
+
+}
+
+git_and_build_hwloc() {
+       cd "$GITDIR"
+       git clone https://github.com/protomens/hwloc
+       cd hwloc
+        ./autogen.sh && ./configure &&  make
+
+}
+
+update_xmrig() {
+        if [ -f "xmrig" ]; then
+                echo "Removing old soft-link..."
+                rm xmrig
+                sleep 1
+                echo "Done."
+        fi
+
+        cd "$GITDIR"
+        
+        if [ -d "$XMRIGDIR" ]; then
+                rm -rf xmrig
+        fi
+        
+        if [ ! -d "$HWLOCDIR" ]; then
+                git_and_build_hwloc
+        fi
+      
+        if [[ $BETA -eq 1 ]]; then          
+                git clone https://github.com/MoneroOcean/xmrig
+        else
+                git clone https://github.com/SIEVEofEratosthenes/xmrig
+        fi
+        
+        cd "$XMRIGDIR" && mkdir build && cd build
+        cmake .. -DHWLOC_INCLUDE_DIR=/data/data/com.termux/files/home/git/hwloc/include -DHWLOC_LIBRARY=/data/data/com.termux/files/home/git/hwloc/hwloc/.libs/libhwloc.so
+        make
+        echo "Done."
+        
+        cd /data/data/com.termux/files/home
+        ln -s /data/data/com.termux/files/home/git/xmrig/build/xmrig xmrig
+        echo "Run: nice ./xmrig -c <config_file>"
+        
+}
+
+
+fresh_install() {
+        echo "We need to change the termux repo. Select (grimler) for best results on main repo"
+        echo -ne "(Press enter to continue): "
+        read blah
+        sleep 1
+        termux-change-repo
+        echo " "
+        echo -ne "Installing dependencies: (autoconf automake cmake git libtool)..."
+        sleep 2
+        apt-get -q -y install autoconf automake cmake git libtool 
+        echo "Done."
+        echo "Getting gits: (hwloc, xmrig, sieve-rig)..."
+        mkdir git && cd git
+
+        if [[ $BETA -eq 1 ]]; then          
+                git clone https://github.com/MoneroOcean/xmrig
+        else
+                git clone https://github.com/SIEVEofEratosthenes/xmrig
+        fi
+
+        git clone https://github.com/zetakov/sieve-rig
+        git_and_build_hwloc
+        echo "Done"
+        cd "$XMRIGDIR" && mkdir build && cd build
+        cmake .. -DHWLOC_INCLUDE_DIR=/data/data/com.termux/files/home/git/hwloc/include -DHWLOC_LIBRARY=/data/data/com.termux/files/home/git/hwloc/hwloc/.libs/libhwloc.so
+        make
+        cp ../../sieve-rig/config.json .
+        sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:20032",/' ./config.json
+        echo -ne "XMR Address: "
+        read XMRADDRESS
+        sed -i 's/"user": *"[^"]*",/"user": "'$XMRADDRESS'",/' ./config.json
+        echo -ne "Worker Name: "
+        read WORKER
+        sed -i 's/"pass": *"[^"]*",/"pass": "'"$WORKER"'",/' ./config.json
+        sed -i 's/"tls": false,/"tls": true,/' ./config.json
+        sed '/"tls":.*$/{n;s/false/true/}' -i ./config.json
+        sed -i 's/"mode": *"[^"]*",/"mode": "lite",/' ./config.json
+        cd /data/data/com.termux/files/home
+        ln -s git/xmrig/build/xmrig xmrig
+        echo "If all went as planned we will run xmrig-mo with benchmarking. If termux exits after benchmarking, either restart temux and xmrig or reboot the phone. This has to do with memory heaps being garbled during benchmarking."
+        echo " "
+        echo "Running xmrig and benchmarking...."
+        sleep 6
+        ./xmrig
+
+}
+
+
+while [ "$#" -gt 0 ]; do
+        key=${1}
+
+        case ${key} in
+                --update)
+                        UPDATE=1
+                        shift
+                        ;;
+                --mo)
+                        BETA=1
+                        shift
+                        ;;
+                -h|--help)
+                        help_screen
+                        shift
+                        ;;
+
+                *)
+                        shift
+                        ;;
+        esac
+done
+
+
+if [[ $UPDATE -eq 1 ]]; then
+        update_xmrig
+else
+        fresh_install
+fi
+
 
